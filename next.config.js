fd865cddf1af0b3bf0f8692065270a301f2afdea
Diya Vijay <diyavijay2371@gmail.com>
2024-02-15 17:47:06 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
-    //Other configurations
-    output: "export", // This option must be set for static exports.
-};
+  reactStrictMode: true,
+
+ // Add basePath
+  basePath: '/app',
+
+}
+module.exports = nextConfig
