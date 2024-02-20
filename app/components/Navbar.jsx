2fc4e6fe8271e7296a0687b170c10a3e79a96388
DiyaVj <diyavijay2371@gmail.com>
2024-02-20 13:06:51 +0530

    gitignore updated

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -40,37 +40,32 @@ const Navbar = () => {
         >
           DV
         </Link>
-        <div className="mobile-menu block md:hidde">
-          {!navbarOpen ? (
-            <button
-              onClick={() => setNavbarOpen(true)}
-              className="flex items-center px-3 py-2 hover:text-white"
-            >
-              <Bars3Icon className="h-5 w-5" />
-            </button>
-          ) : (
-            <button
-              onClick={() => setNavbarOpen(false)}
-              className="flex items-center px-3 py-2 text-slate-200 hover:text-white"
-            >
-              <XMarkIcon className="h-5 w-5" />
-            </button>
-          )}
-        </div>
+        <div className="mobile-menu block md:hidden">
+                    {
+                        !navbarOpen ? 
+                        <button onClick={() => setNavbarOpen(true)} className="flex items-center px-3 py-2 hover:text-white">
+                            <Bars3Icon className="h-5 w-5" />
+                        </button> : 
+                       <button onClick={() => setNavbarOpen(false)} className="flex items-center px-3 py-2 text-slate-200 hover:text-white">
+                            <XMarkIcon className="h-5 w-5" />
+                        </button>
+                    }
+                </div>
 
-        <div className="menu hidden md:block md:w-auto" id="navbar">
-          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-1">
-            {navLinks.map((link, index) => (
-              <li key={index}>
-                <NavLink href={link.path} title={link.title} />
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
+                <div className="menu hidden md:block md:w-auto" id="navbar">
+                    <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-1">
+                        {navLinks.map((link,index) => (
+                            <li key={index}>
+                                <NavLink href={link.path} title={link.title} />
+                            </li>
+                        ))}
+                    </ul>
 
-      {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
-    </nav>
+                </div>
+            </div>
+
+            {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
+        </nav>
   );
 };
 
